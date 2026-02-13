d19f821252f957aa2d827430c28a7fae0d5fc110
priyankasatija05-star <priyankasatija05@gmail.com>
2026-02-13 13:11:45 +0530

    code chnages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import './App.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Dashboard from './components/Dashboard/Dashboard';
-import SignUp from './components/Signup/SignUp';
-import Login from './components/Login/Login';
+// import Dashboard from './components/Dashboard/Dashboard';
+// import SignUp from './components/Signup/SignUp';
+// import Login from './components/Login/Login';
 import { Route, Routes } from 'react-router-dom';
-import { store } from './store';
+// import { store } from './store';
 import { Provider } from "react-redux";
 const theme = createTheme();
 function App() {
   return (
-    <Provider store={store}>
+    // <Provider store={store}>
   <ThemeProvider theme={theme}>
        <Routes>
-      <Route path="/" element={<Login />} />
+      {/* <Route path="/" element={<Login />} />
       <Route path="/Signup" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} /> */}
     </Routes>
      </ThemeProvider>
-     </Provider>
+    //  </Provider>
   );
 }
 
